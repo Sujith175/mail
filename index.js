f78bde50652b7725adc0a65f19f2901299e962d6
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2024-06-22 14:19:00 +0530

    new mail

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
 const mailRouter = require("./Controllers/mail");
+
+const serverless = require("serverless-http");
+
 require("dotenv").config(); // Ensure dotenv is called as a function
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
 // Use the mail router for specific paths
-app.use("/api/users", mailRouter);
+// app.use("/api/users", mailRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+app.use("/api/users", mailRouter);
+
+module.exports.handler = serverless(app);
